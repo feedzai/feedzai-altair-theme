b42cfc6fb5b8a4b87bf8637af08d8c4402f6bac1
João Palmeiro <joaommpalmeiro@gmail.com>
2022-04-21 20:55:37 +0100

    Update tokens.py

--- feedzai_altair_theme/tokens.py
+++ feedzai_altair_theme/tokens.py
@@ -1,1 +1,9 @@
 """Design tokens for the theme and standalone use."""
+
+from typing import Dict
+
+FONT: str = "Roboto, Arial, sans-serif"
+
+FONT_SIZES: Dict[str, int] = {"sm": 12, "md": 16, "lg": 20}
+
+OPACITIES: Dict[str, float] = {"md": 0.5}
