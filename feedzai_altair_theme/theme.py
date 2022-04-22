04e5a267da53dc5683839069209b77dbd0bdce17
João Palmeiro <joaommpalmeiro@gmail.com>
2022-04-22 11:04:18 +0100

    Update theme config

--- feedzai_altair_theme/theme.py
+++ feedzai_altair_theme/theme.py
@@ -1,6 +1,15 @@
 """Altair theme configuration."""
 
+from .tokens import COLORS
+
 
 def feedzai():
     """Feedzai theme (light theme)."""
-    return {"config": {}}
+    return {
+        "config": {
+            "axis": {
+                "domain": True,
+                "domainColor": COLORS["axis"],
+            }
+        }
+    }
